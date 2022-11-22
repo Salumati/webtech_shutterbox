1f62b423f11417f415dac4f5c4a71419790ec49a
Sarah Tiefert <sarah.tiefert@htwg-konstanz.de>
2022-11-22 06:23:53 +0100

    game now highlights or hides certain buttons depending on game status

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,9 @@
 const diceOutputID = "dice-output";
+const undoID = "undo";
+const redoID = "redo";
+const diceID = "dice";
+const toGameID = "toGame";
+const changeStylesID = "styles";
 
 
 function loadGame(numberOfMoves, sum){
@@ -6,16 +11,30 @@ function loadGame(numberOfMoves, sum){
     startGame();
   }else if(sum === 0){
     document.getElementById(diceOutputID).innerHTML = "No more moves, pleas throw the dice again."
+    highlightButton(diceID)
   }
-
+  hideButton(toGameID);
+  hideButton(changeStylesID);
 }
 
 function startGame(){
     sayHello();
+    hideButton(undoID);
+    hideButton(redoID);
+    highlightButton(diceID);
 }
 function sayHello(){
   document.getElementById(diceOutputID).innerHTML = ("Welcome! To start, please throw the dice.") ;
   // highlight the dice button in the first move!
+}
+
+function highlightButton(buttonID){
+  document.getElementById(buttonID).style.color = "red";
+
+}
+function hideButton(buttonID){
+  document.getElementById(buttonID).style.display = "none";
+
 }
 
 function closeClap(index, sum){
