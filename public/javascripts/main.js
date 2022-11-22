869aa168def82636d1ca9dcfccf4d56a89b48256
Sarah Tiefert <sarah.tiefert@htwg-konstanz.de>
2022-11-22 09:48:04 +0100

    created set up for highlighten current player, but couldn't make it work properly yet

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,6 +4,9 @@ const redoID = "redo";
 const diceID = "dice";
 const toGameID = "toGame";
 const changeStylesID = "styles";
+const player1ID = "p1";
+const player2ID = "p2";
+const currentPlayerID = "curP";
 
 
 function loadGame(numberOfMoves, sum){
@@ -30,11 +33,13 @@ function sayHello(){
 
 function highlightButton(buttonID){
   document.getElementById(buttonID).style.color = "red";
-
 }
 function hideButton(buttonID){
   document.getElementById(buttonID).style.display = "none";
-
+}
+function highlightText(id){
+  document.getElementById(id).style.color = "blue";
+  document.getElementById(id).style.fontWeight = "bold";
 }
 
 function closeClap(index, sum){
@@ -48,6 +53,27 @@ function throwDice(sum){
   if(sum > 0)
   {
     window.alert("please close claps first. you still have moves left");
+  }
+}
+
+function markActivePlayer(playerStr){
+  let playerArray = playerStr.split("|");
+  let p1Display = playerArray(0);
+  let p2Display = playerArray(1);
+  let turn = playerArray(2);
+
+  let p1 = p1Display.split(":")(0);
+  let p2 = p2Display.split(":")(0);
+  let currentPlayer = turn.split("'")(0);
+
+  document.getElementById(player1ID).innerHTML = p1Display;
+  document.getElementById(player2ID).innerHTML = p2Display;
+  document.getElementById(currentPlayerID).innerHTML = turn;
+
+  if(currentPlayer === p1){
+    highlightText(player1ID);
+  } else{
+    highlightText(player2ID);
   }
 }
 
